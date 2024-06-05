34f66943fe27de3474ce8e0af9984f0e625043ea
pyopywhiz <acc0568074190@gmail.com>
2024-06-06 00:56:49 +0700

    Change form name

---
+++ FNFNewBot/MainForm.cs
@@ -0,0 +1,15 @@
+namespace FNFNewBot
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
