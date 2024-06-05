eaeac7077ab135e226a7aaf8aaa4284e2e184e3e
pyopywhiz <acc0568074190@gmail.com>
2024-06-06 01:35:09 +0700

    Add choose folder button

--- FNFNewBot/MainForm.Designer.cs
+++ FNFNewBot/MainForm.Designer.cs
@@ -28,19 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            textBoxFolder = new TextBox();
+            buttonChooseFolder = new Button();
+            treeViewJsonFile = new TreeView();
+            logTextBox = new RichTextBox();
             SuspendLayout();
+            // 
+            // textBoxFolder
+            // 
+            textBoxFolder.Location = new Point(12, 12);
+            textBoxFolder.Name = "textBoxFolder";
+            textBoxFolder.Size = new Size(275, 23);
+            textBoxFolder.TabIndex = 0;
+            // 
+            // buttonChooseFolder
+            // 
+            buttonChooseFolder.Location = new Point(293, 12);
+            buttonChooseFolder.Name = "buttonChooseFolder";
+            buttonChooseFolder.Size = new Size(141, 23);
+            buttonChooseFolder.TabIndex = 1;
+            buttonChooseFolder.Text = "Choose Folder";
+            buttonChooseFolder.UseVisualStyleBackColor = true;
+            // 
+            // treeViewJsonFile
+            // 
+            treeViewJsonFile.Location = new Point(12, 163);
+            treeViewJsonFile.Name = "treeViewJsonFile";
+            treeViewJsonFile.Size = new Size(422, 275);
+            treeViewJsonFile.TabIndex = 2;
+            // 
+            // logTextBox
+            // 
+            logTextBox.Location = new Point(440, 12);
+            logTextBox.Name = "logTextBox";
+            logTextBox.Size = new Size(348, 426);
+            logTextBox.TabIndex = 3;
+            logTextBox.Text = "";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(logTextBox);
+            Controls.Add(treeViewJsonFile);
+            Controls.Add(buttonChooseFolder);
+            Controls.Add(textBoxFolder);
             Name = "MainForm";
             Text = "FNF New Version Bot";
             Load += MainForm_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBoxFolder;
+        private Button buttonChooseFolder;
+        private TreeView treeViewJsonFile;
+        private RichTextBox logTextBox;
     }
 }
