8a07fdec4cdf47ed6e6376651e07f5c0a6efb763
pyopywhiz <acc0568074190@gmail.com>
2024-06-07 02:48:52 +0700

    Add ui

--- FNFNewBot/MainForm.Designer.cs
+++ FNFNewBot/MainForm.Designer.cs
@@ -32,6 +32,16 @@
             buttonChooseFolder = new Button();
             treeViewJsonFiles = new TreeView();
             logTextBox = new RichTextBox();
+            comboBoxDifficulty = new ComboBox();
+            textBoxKeyMap = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            nUDSalt = new NumericUpDown();
+            nUDWaiting = new NumericUpDown();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)nUDSalt).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nUDWaiting).BeginInit();
             SuspendLayout();
             // 
             // textBoxFolder
@@ -54,9 +64,9 @@
             // 
             // treeViewJsonFiles
             // 
-            treeViewJsonFiles.Location = new Point(12, 242);
+            treeViewJsonFiles.Location = new Point(12, 163);
             treeViewJsonFiles.Name = "treeViewJsonFiles";
-            treeViewJsonFiles.Size = new Size(346, 196);
+            treeViewJsonFiles.Size = new Size(346, 275);
             treeViewJsonFiles.TabIndex = 2;
             treeViewJsonFiles.NodeMouseDoubleClick += treeViewJsonFiles_NodeMouseDoubleClick;
             // 
@@ -68,11 +78,91 @@
             logTextBox.TabIndex = 3;
             logTextBox.Text = "";
             // 
+            // comboBoxDifficulty
+            // 
+            comboBoxDifficulty.FormattingEnabled = true;
+            comboBoxDifficulty.Location = new Point(96, 43);
+            comboBoxDifficulty.Name = "comboBoxDifficulty";
+            comboBoxDifficulty.Size = new Size(262, 23);
+            comboBoxDifficulty.TabIndex = 4;
+            // 
+            // textBoxKeyMap
+            // 
+            textBoxKeyMap.Location = new Point(96, 72);
+            textBoxKeyMap.Name = "textBoxKeyMap";
+            textBoxKeyMap.Size = new Size(262, 23);
+            textBoxKeyMap.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Difficulty";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Key map";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 104);
+            label3.Name = "label3";
+            label3.Size = new Size(26, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Salt";
+            // 
+            // nUDSalt
+            // 
+            nUDSalt.Increment = new decimal(new int[] { 50, 0, 0, 0 });
+            nUDSalt.Location = new Point(96, 102);
+            nUDSalt.Maximum = new decimal(new int[] { 1000, 0, 0, 0 });
+            nUDSalt.Minimum = new decimal(new int[] { 1000, 0, 0, int.MinValue });
+            nUDSalt.Name = "nUDSalt";
+            nUDSalt.Size = new Size(262, 23);
+            nUDSalt.TabIndex = 10;
+            nUDSalt.TextAlign = HorizontalAlignment.Center;
+            // 
+            // nUDWaiting
+            // 
+            nUDWaiting.Increment = new decimal(new int[] { 100, 0, 0, 0 });
+            nUDWaiting.Location = new Point(96, 131);
+            nUDWaiting.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            nUDWaiting.Name = "nUDWaiting";
+            nUDWaiting.Size = new Size(262, 23);
+            nUDWaiting.TabIndex = 12;
+            nUDWaiting.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(48, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Waiting";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(nUDWaiting);
+            Controls.Add(label4);
+            Controls.Add(nUDSalt);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxKeyMap);
+            Controls.Add(comboBoxDifficulty);
             Controls.Add(logTextBox);
             Controls.Add(treeViewJsonFiles);
             Controls.Add(buttonChooseFolder);
@@ -81,6 +171,8 @@
             Text = "FNF New Version Bot";
             FormClosing += MainForm_FormClosing;
             Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)nUDSalt).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nUDWaiting).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -91,5 +183,13 @@
         private Button buttonChooseFolder;
         private TreeView treeViewJsonFiles;
         private RichTextBox logTextBox;
+        private ComboBox comboBoxDifficulty;
+        private TextBox textBoxKeyMap;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private NumericUpDown nUDSalt;
+        private NumericUpDown nUDWaiting;
+        private Label label4;
     }
 }
